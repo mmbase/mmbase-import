970dcb3c1d873f66974bd8338e517a698beabbc3
Pierre van Rooden <pvrooden@gmail.com>
2002-06-28 10:36:09 +0000

    pierre minor fix in Dove (return guiname in proper place in the xml response)

--- src/org/mmbase/applications/dove/Dove.java
+++ src/org/mmbase/applications/dove/Dove.java
@@ -49,7 +49,7 @@ import org.mmbase.bridge.*;
  *
  * @author Pierre van Rooden
  * @since MMBase-1.5
- * @version $Id: Dove.java,v 1.1.2.3 2002-03-18 16:53:15 pierre Exp $
+ * @version $Id: Dove.java,v 1.1.2.4 2002-06-28 10:36:09 pierre Exp $
  */
 
 public class Dove extends AbstractDove {
@@ -501,7 +501,7 @@ public class Dove extends AbstractDove {
                         field.setAttribute(ELM_NAME,fname);
                         fields.appendChild(field);
                         // guiname (XXX:language is ignored)
-                        elm=addContentElement(GUINAME,fielddef.getGUIName(),out);
+                        elm=addContentElement(GUINAME,fielddef.getGUIName(),field);
                         if (lang!=null) elm.setAttribute(ELM_LANG,lang);
                         // guitype
                         String guitype=fielddef.getGUIType();
