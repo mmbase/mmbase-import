e1a734f7d9c7cec12f7003477f9b0bdcf32540ee
Michiel Meeuwissen <git@meeuw.org>
2003-02-24 15:28:21 +0000

    fix for #5872

--- applications/taglib/src/org/mmbase/bridge/jsp/taglib/WriteTag.java
+++ applications/taglib/src/org/mmbase/bridge/jsp/taglib/WriteTag.java
@@ -13,6 +13,7 @@ import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.JspException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -30,6 +31,7 @@ import org.mmbase.util.logging.Logging;
 public class WriteTag extends ContextReferrerTag implements Writer {
 
     public static int MAX_COOKIE_AGE = 60*60*24*30*6; // half year
+    public static String COOKIE_PATH    = "/";
     private static Logger log = Logging.getLoggerInstance(WriteTag.class.getName());
 
     protected WriterHelper helper = new WriterHelper();
@@ -107,19 +109,46 @@ public class WriteTag extends ContextReferrerTag implements Writer {
         }
         if (cookie != null) {
             Object v = helper.getValue();
-            Cookie c;
-            if (v instanceof String) {
-                c = new Cookie(cookie, (String) v);
-            } else if (v instanceof Integer) {
-                c = new Cookie(cookie, "" + v);
-            } else if (v instanceof Node) {
-                c = new Cookie(cookie, "" + ((Node) v).getNumber());
+            String cookievalue;
+            if (v instanceof Node) {
+                cookievalue = "" + ((Node) v).getNumber();
+            } else if (v instanceof String || v instanceof Number) {
+                cookievalue =  "" + v;
             } else {
                 throw new JspTagException(v.toString() + " is not of the right type to write to cookie. It is a (" +  v.getClass().getName() + ")");
             }
-            c.setMaxAge(MAX_COOKIE_AGE);
-            c.setPath("/");
-            ((HttpServletResponse)pageContext.getResponse()).addCookie(c);
+
+            // remove all cookies with given name
+            HttpServletRequest request = ((HttpServletRequest)pageContext.getRequest());
+            HttpServletResponse response = ((HttpServletResponse)pageContext.getResponse());
+            
+            if (log.isDebugEnabled()) {
+                log.debug("Writing cookie " + cookie + " / " + v);
+            }
+            // count present cookies of this name
+            int cookiecount = 0;
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) { 
+                for (int i=0; i< cookies.length; i++) {
+                    Cookie c = cookies[i];
+                    if (c.getName().equals(cookie)) {
+                        cookiecount++;
+                    }
+                }
+            }
+
+
+            {  // on root (keep things simple)
+                Cookie c = new Cookie(cookie, cookievalue);
+                c.setPath(COOKIE_PATH);               
+                c.setMaxAge(MAX_COOKIE_AGE);
+                response.addCookie(c);
+            }
+            if (cookiecount > 1) { //also in current dir (in case it was there already)
+                Cookie c = new Cookie(cookie, cookievalue);
+                c.setMaxAge(MAX_COOKIE_AGE);
+                response.addCookie(c);
+            }
             helper.overrideWrite(false);
         }
         return EVAL_BODY_BUFFERED;
