fea141c6d6563e95536b5e8e1f10712da0109e4d
Pierre van Rooden <pvrooden@gmail.com>
2003-04-10 07:53:05 +0000

    pierre: bugfix #5984  Incomplete files generated by Application backup tools. The application in 1.6 exports old builderformats to support import on older systems. The 'name' attribute' was missing but has been added in 1.6.4.

--- src/org/mmbase/util/XMLEntityResolver.java
+++ src/org/mmbase/util/XMLEntityResolver.java
@@ -24,7 +24,7 @@ import org.mmbase.util.logging.*;
  *
  * @author Gerard van Enk
  * @author Michiel Meeuwissen
- * @version $Id: XMLEntityResolver.java,v 1.18.2.7 2003-03-20 12:30:29 michiel Exp $
+ * @version $Id: XMLEntityResolver.java,v 1.18.2.8 2003-04-10 07:53:04 pierre Exp $
  */
 public class XMLEntityResolver implements EntityResolver {
 
@@ -62,15 +62,16 @@ public class XMLEntityResolver implements EntityResolver {
 
     static {
         // This list is not yet complete
-        publicIDtoResource.put("-//MMBase/DTD builder config 1.1//EN",  new Resource(XMLBasicReader.class,    "builder_1_1.dtd"));
-        publicIDtoResource.put("-//MMBase/DTD builder config 1.0//EN",  new Resource(XMLBasicReader.class,    "builder_1_0.dtd"));
-        publicIDtoResource.put("//MMBase - builder//",                  new Resource(XMLBasicReader.class,    "builder_1_1.dtd"));
-        publicIDtoResource.put("-//MMBase/DTD module config 1.0//EN",   new Resource(XMLModuleReader.class,   "module_1_0.dtd"));
-        publicIDtoResource.put("-//MMBase/ DTD module config 1.0//EN",  new Resource(XMLModuleReader.class,   "module_1_0.dtd"));
-        publicIDtoResource.put("-//MMBase//DTD util config 1.0//EN",    new Resource(org.mmbase.util.xml.UtilReader.class,     "util_1_0.dtd"));
+        publicIDtoResource.put("-//MMBase/DTD builder config 1.1//EN",  new Resource(XMLBasicReader.class, "builder_1_1.dtd"));
+        publicIDtoResource.put("-//MMBase//DTD builder config 1.1//EN",  new Resource(XMLBasicReader.class, "builder_1_1.dtd"));
+        publicIDtoResource.put("-//MMBase/DTD builder config 1.0//EN",  new Resource(XMLBasicReader.class, "builder_1_0.dtd"));
+        publicIDtoResource.put("//MMBase - builder//",                  new Resource(XMLBasicReader.class, "builder_1_1.dtd"));
+        publicIDtoResource.put("-//MMBase/DTD module config 1.0//EN",   new Resource(XMLModuleReader.class, "module_1_0.dtd"));
+        publicIDtoResource.put("-//MMBase/ DTD module config 1.0//EN",  new Resource(XMLModuleReader.class, "module_1_0.dtd"));
+        publicIDtoResource.put("-//MMBase//DTD util config 1.0//EN",    new Resource(org.mmbase.util.xml.UtilReader.class, "util_1_0.dtd"));
         publicIDtoResource.put("-//MMBase/DTD database config 1.1//EN", new Resource(XMLDatabaseReader.class, "database_1_1.dtd"));
         publicIDtoResource.put("-//MMBase/DTD application config 1.0//EN", new Resource(XMLApplicationReader.class, "application_1_0.dtd"));
-        publicIDtoResource.put("-//MMBase//DTD builder transactions 1.0//EN", new Resource(org.mmbase.module.TransactionHandler.class,    "transactions_1_0.dtd"));
+        publicIDtoResource.put("-//MMBase//DTD builder transactions 1.0//EN", new Resource(org.mmbase.module.TransactionHandler.class, "transactions_1_0.dtd"));
     }
 
 
