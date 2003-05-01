03d76db9f42998a68580b08e453db5bb06b49b9e
vpro <vpro@91430c5f-d668-0410-9e7d-dd663366dcfd>
2003-05-01 17:28:08 +0000

    pierre: Wish #6037 Desired: searchbox in the editwizard list.jsp Allow for session to also store search data

--- src/org/mmbase/applications/editwizard/Config.java
+++ src/org/mmbase/applications/editwizard/Config.java
@@ -24,7 +24,7 @@ import org.mmbase.util.logging.*;
  *
  * @author  Michiel Meeuwissen
  * @since   MMBase-1.6
- * @version $Id: Config.java,v 1.26.2.3 2003-04-28 14:46:23 michiel Exp $
+ * @version $Id: Config.java,v 1.26.2.4 2003-05-01 17:28:08 vpro Exp $
  */
 
 public class Config {
@@ -76,6 +76,13 @@ public class Config {
         public String constraints;
         public String orderBy;
         public String directions;
+        public String searchDir;
+
+        public String searchFields;
+        public String searchValue="";
+        public String searchType="like";
+        public String baseConstraints;
+
         public int    age = -1;
         public int    start = 0;
         public boolean distinct;
