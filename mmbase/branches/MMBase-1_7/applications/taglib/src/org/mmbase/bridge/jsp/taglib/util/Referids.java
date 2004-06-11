3546d9b57acd8b2bc286a650719a7a6748d2a1fd
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-06-11 08:37:35 +0000

    it's better to have a fixed order when using 'referids', especially for mm:url, otherwise urls are not fully determined by how you specify them. Bug 6492.

--- mmbase/branches/MMBase-1_7/applications/taglib/src/org/mmbase/bridge/jsp/taglib/util/Referids.java
+++ mmbase/branches/MMBase-1_7/applications/taglib/src/org/mmbase/bridge/jsp/taglib/util/Referids.java
@@ -21,14 +21,14 @@ import org.mmbase.util.logging.*;
  * A helper class to implement referids attribute.
  *
  * @author Michiel Meeuwissen
- * @version $Id: Referids.java,v 1.2.2.1 2004-05-07 13:40:51 pierre Exp $
+ * @version $Id: Referids.java,v 1.2.2.2 2004-06-11 08:37:35 michiel Exp $
  * @since MMBase-1.7
  */
 public class  Referids  {
     private static final Logger log = Logging.getLoggerInstance(Referids.class);
 
     public static Map getReferids(Attribute referids, ContextReferrerTag tag) throws JspTagException {
-        Map result = new HashMap();
+        Map result = new LinkedHashMap(); // using a 'linked' hash map ensures that the key/value pairs are added in the same order as there are in the referids attribute
         // log.info("" + referids + " : " + referids.getList(this));
         Iterator i = referids.getList(tag).iterator();
         while (i.hasNext()) {
