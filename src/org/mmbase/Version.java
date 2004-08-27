dfdb6da0d3ebf06cdb1c0e5fc9d95bedf2e8c0a5
Gerard van Enk <gvenk@millionpieces.nl>
2004-08-27 14:21:34 +0000

    Let's make it Final

--- src/org/mmbase/Version.java
+++ src/org/mmbase/Version.java
@@ -17,7 +17,7 @@ import java.io.*;
  *
  * @author Daniel Ockeloen
  * @author Michiel Meeuwissen
- * @version $Id: Version.java,v 1.28.2.3 2004-08-27 14:17:33 gerard Exp $
+ * @version $Id: Version.java,v 1.28.2.4 2004-08-27 14:21:34 gerard Exp $
  */
 public class Version {
 
@@ -95,7 +95,7 @@ public class Version {
      * @since MMBase-1.7
      */
     public static String getReleaseStatus() {
-        return "rc1";
+        return "yes";
     };
 
     /**
