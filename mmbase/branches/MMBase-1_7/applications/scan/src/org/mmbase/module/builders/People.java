7a6281160852e1210e68f91b4cdaf50969326e28
marcel <marcel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-09-27 13:35:46 +0000

    marcel: bugfix #6583: NullPointerException when key == null

--- mmbase/branches/MMBase-1_7/applications/scan/src/org/mmbase/module/builders/People.java
+++ mmbase/branches/MMBase-1_7/applications/scan/src/org/mmbase/module/builders/People.java
@@ -21,7 +21,7 @@ import org.mmbase.util.logging.*;
  *
  * @application Basic [builder]
  * @author Daniel Ockeloen
- * @version $Id: People.java,v 1.7 2003-09-01 13:29:44 pierre Exp $
+ * @version $Id: People.java,v 1.7.2.1 2004-09-27 13:35:46 marcel Exp $
  */
 public class People extends MMObjectBuilder {
 
@@ -103,6 +103,7 @@ public class People extends MMObjectBuilder {
      * also signals the session module
      */
     public void flushCache(String key) {
-        peopleCache.remove(key);
+        if(key!=null) 
+            peopleCache.remove(key);
     }
 }
