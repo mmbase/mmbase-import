98032ae79c57a3a33fdd47af9a5848ff8de97a9e
marcel <marcel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2004-10-21 10:02:43 +0000

    marcel: #6609:  Verify does not show the field it is actually looking for

--- src/org/mmbase/storage/implementation/database/DatabaseStorageManager.java
+++ src/org/mmbase/storage/implementation/database/DatabaseStorageManager.java
@@ -28,7 +28,7 @@ import org.mmbase.util.logging.*;
  *
  * @author Pierre van Rooden
  * @since MMBase-1.7
- * @version $Id: DatabaseStorageManager.java,v 1.63.2.9 2004-08-26 12:40:33 michiel Exp $
+ * @version $Id: DatabaseStorageManager.java,v 1.63.2.10 2004-10-21 09:59:52 marcel Exp $
  */
 public class DatabaseStorageManager implements StorageManager {
 
@@ -1771,7 +1771,13 @@ public class DatabaseStorageManager implements StorageManager {
                     Object id = field.getStorageIdentifier();
                     Map colInfo = (Map)columns.get(id);
                     if ((colInfo == null)) {
-                        log.error("VERIFY: Field '" + field.getDBName() + "' of builder '" + builder.getTableName() + "' does NOT exist in storage! Field will be considered virtual.");
+
+                        // bug #6609, marcel
+                        if((field!=null) && (field.getDBName()!=null) && (field.getDBName().equals(id))) 
+                            log.error("VERIFY: Field '" + field.getDBName() + "' of builder '" + builder.getTableName() + "' does NOT exist in storage! Field will be considered virtual");
+                        else 
+                            log.error("VERIFY: Field '" + field.getDBName() + "' (mapped to field '"+id+"') of builder '" + builder.getTableName() + "' does NOT exist in storage! Field will be considered virtual");
+
                         // set field to virtual so it will not be stored -
                         // prevents future queries or statements from failing
                         field.setDBState(FieldDefs.DBSTATE_VIRTUAL);
