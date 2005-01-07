b3a9158a9d131768be39f598200c48693c0364f6
gerard <gerard@91430c5f-d668-0410-9e7d-dd663366dcfd>
2005-01-07 11:07:03 +0000

    fix for bug #6640:  Jumpers which have the same name as a directory aren't working with Tomcat 5.0.x

--- mmbase/branches/MMBase-1_7/src/org/mmbase/servlet/JumpersFilter.java
+++ mmbase/branches/MMBase-1_7/src/org/mmbase/servlet/JumpersFilter.java
@@ -11,6 +11,8 @@ package org.mmbase.servlet;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.mmbase.module.builders.Jumpers;
 import org.mmbase.module.core.*;
 import org.mmbase.util.logging.*;
@@ -19,7 +21,7 @@ import org.mmbase.util.logging.*;
  * Redirects request based on information supplied by the jumpers builder.
  *
  * @author Jaco de Groot
- * @version $Id: JumpersFilter.java,v 1.9 2004-02-24 11:53:19 michiel Exp $
+ * @version $Id: JumpersFilter.java,v 1.9.2.1 2005-01-07 11:07:03 gerard Exp $
  */
 public class JumpersFilter implements Filter, MMBaseStarter {
     private static final Logger log = Logging.getLoggerInstance(JumpersFilter.class);
@@ -57,7 +59,7 @@ public class JumpersFilter implements Filter, MMBaseStarter {
         // never mind, simply, ignore
     }
     /**
-     * @javadoc
+     * Initializes the filter
      */
     public void init(javax.servlet.FilterConfig filterConfig) throws ServletException {
         name = filterConfig.getFilterName();
@@ -71,7 +73,11 @@ public class JumpersFilter implements Filter, MMBaseStarter {
     }
 
     /**
-     * @javadoc
+     * Filters the request: tries to find a jumper and redirects to this url when found, otherwise the 
+     * request will be handled somewhere else in the filterchain.
+     * @param servletRequest The ServletRequest.
+     * @param servletResponse The ServletResponse.
+     * @param filterChain The FilterChain.
      */
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws java.io.IOException, ServletException {
         if (bul == null) {
@@ -92,15 +98,33 @@ public class JumpersFilter implements Filter, MMBaseStarter {
          * The path starts with a "/" character but does not end with a "/" character.
          * For servlets in the default (root) context, this method returns "". The container does not decode this string.
          */
-        int contextPart = req.getContextPath().length();
+        String context = "";
+        context = req.getContextPath();
+        int contextPart = context.length();
         String reqURI = req.getRequestURI();
         String key = "";
         if (contextPart < reqURI.length()) {
             // also remove the leading "/", unless it's an empty string.
-            key = req.getRequestURI().substring(req.getContextPath().length()+1);
+            key = req.getRequestURI().substring(context.length()+1);
         }
 
-        if (key.indexOf('.') == -1 && !key.endsWith("/")) {
+        if (log.isDebugEnabled()) {
+            log.debug("contextpath is: " + context);
+            log.debug("key is: " + key);
+            log.debug("uri is: " + reqURI);
+        }
+        
+        //ignore keys with extensions
+        if (key.indexOf('.') == -1 ) {
+            // because Tomcat version > 5.0.5 always adds a trailing slash if
+            // there's a directory with the same name, the trailing slash must be removed
+            if (key.endsWith("/")) {
+                key = key.substring(0,key.length()-1);
+                if (log.isDebugEnabled()){
+                    log.debug("after removing trailing slash key becomes: " + key);
+                }
+            }
+            //get jumper from Jumpers builder
             String url = bul.getJump(key);
             if (url != null) {
                 /*
@@ -113,7 +137,27 @@ public class JumpersFilter implements Filter, MMBaseStarter {
                  *  After using this method, the response should be considered to be committed and should not be written to.
                  */
                 // res.sendRedirect(res.encodeRedirectURL(req.getContextPath() + url));
-                res.sendRedirect(url);
+
+                // there can be different types of jumper uris which all must behandled
+                // differently:
+                // - absolute with a scheme (http://www.servername.nl/context/bla/bla123), muste be redirected
+                // - absolute without a scheme (/bla/bla123), must be redirected
+                // - relative (bla/bla123), must be made absolute and then be redirected
+                URI redirURI = null;
+                try {
+                    redirURI = new URI(url);
+                } catch (URISyntaxException ex) {
+                    log.error ("jumper URI syntax is not valid");
+                }
+                if (redirURI != null) {
+                    if (redirURI.isAbsolute()) {
+                        res.sendRedirect(url);
+                    } else if (url.startsWith("/")) {
+                        res.sendRedirect(url);
+                    } else {
+                        res.sendRedirect(context +"/" + url);
+                    }
+                }
                 return;
             }
         }
@@ -121,7 +165,7 @@ public class JumpersFilter implements Filter, MMBaseStarter {
     }
 
     /**
-     * @javadoc
+     * destroys the filter
      */
     public void destroy() {
         log.info("Filter " + name + " destroyed.");
