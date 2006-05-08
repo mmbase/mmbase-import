7719d9d3c0e68cf2c2485a9a90ba6a5b8d56a241
nobody <nobody@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-05-08 21:52:33 +0000

    This commit was manufactured by cvs2svn to create tag 'MMBase-1_8_0_Final'.

--- src/org/mmbase/Version.java
+++ src/org/mmbase/Version.java
@@ -17,7 +17,7 @@ import java.io.*;
  *
  * @author Daniel Ockeloen
  * @author Michiel Meeuwissen
- * @version $Id: Version.java,v 1.37 2006-05-08 20:54:02 michiel Exp $
+ * @version $Id: Version.java,v 1.36 2006-05-08 20:52:02 michiel Exp $
  */
 public class Version {
 
@@ -53,7 +53,7 @@ public class Version {
      * @since MMBase-1.6
      */
     public static int getPatchLevel() {
-        return 1;
+        return 0;
     }
 
     /**
@@ -94,7 +94,7 @@ public class Version {
      * @since MMBase-1.6
      */
     public static boolean isRelease() {
-        return false;
+        return true;
     };
 
     /**
@@ -103,7 +103,7 @@ public class Version {
      * @since MMBase-1.7
      */
     public static String getReleaseStatus() {
-        return "";
+        return "final";
     };
 
     /**
