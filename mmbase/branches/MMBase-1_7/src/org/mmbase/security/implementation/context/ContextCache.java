4aa50b1bfa681d41b14de6853fa7242121c4b31b
keesj <keesj@91430c5f-d668-0410-9e7d-dd663366dcfd>
2006-07-26 10:24:57 +0000

    keesj:fix for #6853 rights context cache already contained this entry waring, fixed indenting , added beter debug output

--- mmbase/branches/MMBase-1_7/src/org/mmbase/security/implementation/context/ContextCache.java
+++ mmbase/branches/MMBase-1_7/src/org/mmbase/security/implementation/context/ContextCache.java
@@ -6,7 +6,7 @@ OSI Certified is a certification mark of the Open Source Initiative.
 The license (Mozilla version 1.0) can be read at the MMBase site.
 See http://www.MMBase.org/license
 
-*/
+ */
 package org.mmbase.security.implementation.context;
 
 import java.util.HashMap;
@@ -21,99 +21,110 @@ import org.mmbase.util.logging.Logging;
  * @javadoc
  *
  * @author Eduard Witteveen
- * @version $Id: ContextCache.java,v 1.7 2003-03-04 15:29:37 nico Exp $
+ * @version $Id: ContextCache.java,v 1.7.2.1 2006-07-26 10:24:57 keesj Exp $
  */
 public class ContextCache  {
-    private static Logger log = Logging.getLoggerInstance(ContextCache.class.getName());
+	private static Logger log = Logging.getLoggerInstance(ContextCache.class);
 
-    private org.mmbase.cache.Cache globalRightCache = new org.mmbase.cache.Cache(50) {
-        public String getName()        { return "ContextRight"; }
-        public String getDescription() { return "Context Security Implementation Rights Cache"; }
-        };
+	private org.mmbase.cache.Cache globalRightCache = new org.mmbase.cache.Cache(50) {
+		public String getName()        { return "ContextRight"; }
+		public String getDescription() { return "Context Security Implementation Rights Cache"; }
+	};
 
-    private long    rightTries = 0;
-    private long    rightSucces = 0;
-    private long    rightSize = 0;
+	private long    rightTries = 0;
+	private long    rightSucces = 0;
+	private long    rightSize = 0;
 
-    public void rightAdd(String operation, String context, String user, boolean value) {
-        Map operationCache = (Map) globalRightCache.get(operation);
-    // when operation not known, create
-    if(operationCache == null) {
-        operationCache = new HashMap();
-        globalRightCache.put(operation, operationCache);
-    }
-    Map contextCache = (Map) operationCache.get(context);
-    // when context not known, create
-    if(contextCache == null) {
-        contextCache = new HashMap();
-        operationCache.put(context, contextCache);
-    }
-        if(contextCache.containsKey(user)) {
-        log.warn("rights context cache already contained this entry");
-    }
-        contextCache.put(user, new Boolean(value));
-        log.debug("added to cache the operation: " + operation + " for context: " + context + " with user: " + user + " with value: " + value );
-        rightSize++;
-    }
+	public void rightAdd(String operation, String context, String user, boolean value) {
+		//get and put operations on the cache are synchronized but still
+		//it's possible that 2 threads first perform a rightGet that proves
+		//that no entry is in the cache. after that both threads would start
+		//adding operation to the cache one after the other this is why we check here again if the 
+		//operation is in the cache
+		if (rightGet(operation, context, user) != null){
+			if (log.isDebugEnabled()){
+				log.debug("prevented to put a double entry in the cache");
+			}
+			return;
+		}
+		Map operationCache = (Map) globalRightCache.get(operation);
+		// when operation not known, create
+		if(operationCache == null) {
+			operationCache = new HashMap();
+			globalRightCache.put(operation, operationCache);
+		}
+		Map contextCache = (Map) operationCache.get(context);
+		// when context not known, create
+		if(contextCache == null) {
+			contextCache = new HashMap();
+			operationCache.put(context, contextCache);
+		}
+		if(contextCache.containsKey(user)) {
+			log.warn("rights context cache for context " +context + " and operation " + operation + " already contained entry for user " + user + " with value " + contextCache.get(user));
+		}
+		contextCache.put(user, new Boolean(value));
+		log.debug("added to cache the operation: " + operation + " for context: " + context + " with user: " + user + " with value: " + value );
+		rightSize++;
+	}
 
-    public Boolean rightGet(String operation, String context, String user) {
-        HashMap operationCache = (HashMap)globalRightCache.get(operation);
-        rightTries ++;
-    if(operationCache == null) {
-        log.debug("operation not found in cache ("+info(rightTries, rightSucces, rightSize)+")");
-        return null;
-    }
+	public Boolean rightGet(String operation, String context, String user) {
+		HashMap operationCache = (HashMap)globalRightCache.get(operation);
+		rightTries ++;
+		if(operationCache == null) {
+			log.debug("operation "+ operation +" not found in cache ("+info(rightTries, rightSucces, rightSize)+")");
+			return null;
+		}
 
-        HashMap contextCache = (HashMap)operationCache.get(context);
+		HashMap contextCache = (HashMap)operationCache.get(context);
 
-    if(contextCache == null) {
-        log.debug("rights context not found in cache ("+info(rightTries, rightSucces, rightSize)+")");
-        return null;
-    }
+		if(contextCache == null) {
+			log.debug("rights context catche for context "+ context +" not found in cache ("+info(rightTries, rightSucces, rightSize)+")");
+			return null;
+		}
 
-        if(contextCache.containsKey(user)) {
-        rightSucces ++;
-        log.debug("user found in cache ("+info(rightTries, rightSucces, rightSize)+")");
-        log.debug("the operation: " + operation + " for context: " + context + " with user: " + user + " returned: " + contextCache.get(user) );
-        }
-    return (Boolean)contextCache.get(user);
-    }
+		if(contextCache.containsKey(user)) {
+			rightSucces ++;
+			log.debug("user found in cache ("+info(rightTries, rightSucces, rightSize)+")");
+			log.debug("the operation: " + operation + " for context: " + context + " with user: " + user + " returned: " + contextCache.get(user) );
+		}
+		return (Boolean)contextCache.get(user);
+	}
 
-    private org.mmbase.cache.Cache globalContextCache = new org.mmbase.cache.Cache(50) {
-        public String getName()        { return "ContextContext"; }
-        public String getDescription() { return "Context Security Implementation Context Cache"; }
-        };
+	private org.mmbase.cache.Cache globalContextCache = new org.mmbase.cache.Cache(50) {
+		public String getName()        { return "ContextContext"; }
+		public String getDescription() { return "Context Security Implementation Context Cache"; }
+	};
 
-    private long    contextTries = 0;
-    private long    contextSucces = 0;
-    private long    contextSize = 0;
+	private long    contextTries = 0;
+	private long    contextSucces = 0;
+	private long    contextSize = 0;
 
-    public void contextAdd(String context, Set possible) {
-    // when context was already known....
-        if(globalContextCache.containsKey(context)) {
-        log.warn("context cache already contained this entry");
-    }
-        globalContextCache.put(context, possible);
-        log.debug("added possible list to context with name : " + context);
-        contextSize++;
-    }
+	public void contextAdd(String context, Set possible) {
+		// when context was already known....
+		if(globalContextCache.containsKey(context)) {
+			log.warn("context cache already contained this entry");
+		}
+		globalContextCache.put(context, possible);
+		log.debug("added possible list to context with name : " + context);
+		contextSize++;
+	}
 
-    public Set contextGet(String context) {
-        contextTries++;
+	public Set contextGet(String context) {
+		contextTries++;
 
-        if(globalContextCache.containsKey(context)) {
-        contextSucces++;
-        log.debug("context found in cache ("+info(contextTries, contextSucces, contextSize)+")");
-        }
-    return (Set)globalContextCache.get(context);
-    }
+		if(globalContextCache.containsKey(context)) {
+			contextSucces++;
+			log.debug("context found in cache ("+info(contextTries, contextSucces, contextSize)+")");
+		}
+		return (Set)globalContextCache.get(context);
+	}
 
-    private String info(long tries, long succes, long size) {
-        return "hit of #"+succes+ " access of #"+tries+" ("+ succes/(tries/100.0)+" %) with a number of entries #"+size;
-    }
+	private String info(long tries, long succes, long size) {
+		return "hit of #"+succes+ " access of #"+tries+" ("+ succes/(tries/100.0)+" %) with a number of entries #"+size;
+	}
 
-    ContextCache() {
-        globalContextCache.putCache();
-        globalRightCache.putCache();
-    }
+	ContextCache() {
+		globalContextCache.putCache();
+		globalRightCache.putCache();
+	}
 }
