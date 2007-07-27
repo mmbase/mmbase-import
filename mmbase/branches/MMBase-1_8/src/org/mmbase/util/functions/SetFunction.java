701c74f4742a1dc5b822a2eb565c055a43b593dd
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-07-27 13:59:35 +0000

    MMB-1483

--- mmbase/branches/MMBase-1_8/src/org/mmbase/util/functions/SetFunction.java
+++ mmbase/branches/MMBase-1_8/src/org/mmbase/util/functions/SetFunction.java
@@ -20,42 +20,63 @@ import org.mmbase.util.logging.*;
  * @author Michiel Meeuwissen
  * @author Daniel Ockeloen
  * @author Pierre van Rooden
- * @version $Id: SetFunction.java,v 1.13.2.2 2007-04-25 12:57:22 michiel Exp $
+ * @version $Id: SetFunction.java,v 1.13.2.3 2007-07-27 13:59:35 michiel Exp $
  * @since MMBase-1.8
  * @see   FunctionSets
  */
-class SetFunction extends AbstractFunction {
+public class SetFunction extends AbstractFunction {
     private static final Logger log = Logging.getLoggerInstance(SetFunction.class);
 
     /**
      * If type is 'class' the method must be static, or if it is not static, there will ie instantiated <em>one</em> object.
      */
-    public static final int TYPE_CLASS = 1;
+    static final int TYPE_CLASS = 1;
 
     /**
      * If type is 'class' the method must not be static, and on every call to getFunctionValue, a new object is instantiated.
      */
-    public static final int TYPE_INSTANCE = 2;
+    static final int TYPE_INSTANCE = 2;
 
     private Method functionMethod   = null;
     private Object functionInstance = null;
     private int type = TYPE_CLASS;
+    private final int defLength;
 
     public SetFunction(String name, Parameter[] def, ReturnType returnType, String className, String methodName) {
         super(name, def, returnType);
-        initialize(className, methodName);
+        defLength = def.length;
+        Class functionClass;
+        try {
+            functionClass = Class.forName(className);
+        } catch(Exception e) {
+            throw new RuntimeException("Can't create an application function class : " + className + " " + e.getMessage(), e);
+        }
+        initialize(functionClass, methodName);
     }
-    public SetFunction(String name, Parameter[] def ,String className, String methodName) {
+    public SetFunction(String name, Parameter[] def, String className, String methodName) {
+        this(name, def, null, className, methodName);
+    }
+    /**
+     * @since MMBase-1.8.5
+     */
+    public SetFunction(String name, Parameter[] def, Class clazz) {
         super(name, def, null);
-        initialize(className, methodName);
+        defLength = def.length;
+        initialize(clazz, name);
     }
 
     /**
      */
     public Object getFunctionValue(Parameters parameters) {
         parameters.checkRequiredParameters();
+        log.info("Calling " + functionMethod + " with " + parameters);
         try {
-            return functionMethod.invoke(getInstance(), parameters.toArray());
+            if (defLength < parameters.size()) {
+                // when wrapping this fucntion, it can happen that the number of parameters increases.
+                return functionMethod.invoke(getInstance(), parameters.subList(0, defLength).toArray());
+            } else {
+                return functionMethod.invoke(getInstance(), parameters.toArray());
+            }
         } catch (IllegalAccessException iae) {
             log.error("Function call failed (method not available) : " + name +", method: " + functionMethod +
                        ", instance: " + getInstance() +", parameters: " + parameters);
@@ -94,34 +115,10 @@ class SetFunction extends AbstractFunction {
         }
     }
 
-    /**
-     * Initializes the function by creating an instance of the function class, and
-     * locating the method to call.
-     */
-    private void initialize(String className, String methodName) {
-        Class functionClass;
-        try {
-            functionClass = Class.forName(className);
-        } catch(Exception e) {
-            throw new RuntimeException("Can't create an application function class : " + className + " " + e.getMessage(), e);
+    private void checkReturnType() {
+        if (returnType == null) {
+            returnType = new ReturnType(functionMethod.getReturnType(), functionMethod.getReturnType().getClass().getName());
         }
-        try {
-            functionMethod = functionClass.getMethod(methodName, createParameters().toClassArray());
-        } catch(NoSuchMethodException e) {
-            throw new RuntimeException("Function method not found : " + className + "." + methodName + "(" +  Arrays.asList(getParameterDefinition()) +")", e);
-        }
-        if (Modifier.isStatic(functionMethod.getModifiers())) {
-            functionInstance = null;
-        } else {
-            if (type != TYPE_INSTANCE) {
-                try {
-                    functionInstance =  functionMethod.getDeclaringClass().newInstance();
-                } catch(Exception e) {
-                    throw new RuntimeException("Can't create an function instance : " + functionMethod.getDeclaringClass().getName(), e);
-                }
-            }
-        }
-        if (returnType == null) returnType = new ReturnType(functionMethod.getReturnType(), functionMethod.getReturnType().getClass().getName());
 
 	Class returni = functionMethod.getReturnType();
         if (returni.isPrimitive()) {
@@ -147,7 +144,33 @@ class SetFunction extends AbstractFunction {
         }
 	Class returnx = returnType.getDataType().getTypeAsClass();
         if (! returnx.isAssignableFrom(returni)) {
-            log.warn("Return value of function " + className + "." + methodName + " (" + returni + ") does not match method return type as specified in XML: (" + returnx + ")");
+            log.warn("Return value of method " + functionMethod + " (" + returni + ") does not match method return type as specified in XML: (" + returnx + ")");
         }
+    }
+
+    /**
+     * Initializes the function by creating an instance of the function class, and
+     * locating the method to call.
+     */
+    private void initialize(Class functionClass, String methodName) {
+
+        try {
+            functionMethod = functionClass.getMethod(methodName, createParameters().toClassArray());
+        } catch(NoSuchMethodException e) {
+            throw new RuntimeException("Function method not found : " + functionClass + "." + methodName + "(" +  Arrays.asList(getParameterDefinition()) +")", e);
+        }
+        if (Modifier.isStatic(functionMethod.getModifiers())) {
+            functionInstance = null;
+        } else {
+            if (type != TYPE_INSTANCE) {
+                try {
+                    functionInstance =  functionMethod.getDeclaringClass().newInstance();
+                } catch(Exception e) {
+                    throw new RuntimeException("Can't create an function instance : " + functionMethod.getDeclaringClass().getName(), e);
+                }
+            }
+        }
+        checkReturnType();
+
     }
 }
