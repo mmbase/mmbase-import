6ee87d348e2c3cadaf23b2f4573ea9c252bfd59c
nobody <nobody@91430c5f-d668-0410-9e7d-dd663366dcfd>
2007-09-28 12:35:15 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_3_3'.

---
+++ mmbase/tags/v1_3_3/contributions/CMSContainer/cmsc/portal/src/java/com/finalist/cmsc/portalImpl/headerresource/HeaderResource.java
@@ -0,0 +1,16 @@
+package com.finalist.cmsc.portalImpl.headerresource;
+
+public abstract class HeaderResource {
+
+	private boolean dublin;
+	
+	public HeaderResource(boolean dublin) {
+		this.dublin = dublin;
+	}
+
+	public boolean isDublin() {
+		return dublin;
+	}
+
+	public abstract void render(StringBuffer buffer);
+}
