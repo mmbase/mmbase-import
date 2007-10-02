5df000dc4de4244af0807c05ce316e61297cbd66
Michiel Meeuwissen <git@meeuw.org>
2007-10-02 13:32:57 +0000

    MMB-1517

--- applications/media/src/org/mmbase/applications/media/Format.java
+++ applications/media/src/org/mmbase/applications/media/Format.java
@@ -1,8 +1,8 @@
  /*
-  
+
  This software is OSI Certified Open Source Software.
  OSI Certified is a certification mark of the Open Source Initiative.
- 
+
  The license (Mozilla version 1.0) can be read at the MMBase site.
  See http://www.MMBase.org/license
  */
@@ -24,7 +24,7 @@ import org.w3c.dom.Element;
  * Makes the 'Format' constants available.
  *
  * @author Michiel Meeuwissen
- * @version $Id: Format.java,v 1.19 2006-01-13 12:36:23 johannes Exp $
+ * @version $Id: Format.java,v 1.19.2.1 2007-10-02 13:32:57 michiel Exp $
  * @since MMBase-1.7
  */
 // See http://www.javaworld.com/javaworld/jw-07-1997/jw-07-enumerated.html
@@ -34,13 +34,13 @@ public final class Format {   // final class!!
     public final static String RESOURCE = "org.mmbase.applications.media.resources.formats";
     public static final String PUBLIC_ID_MIMEMAPPING_1_0 = "-//MMBase//DTD mimemapping config 1.0//EN";
     public static final String DTD_MIMEMAPPING_1_0       = "mimemapping_1_0.dtd";
-    
+
 
     // in case you want i18ed format strings.
 
     private static Map mimeMapping = null;
     static {
-    
+
         XMLEntityResolver.registerPublicID(PUBLIC_ID_MIMEMAPPING_1_0, DTD_MIMEMAPPING_1_0, Format.class);
 
         File mimeMappingFile = new File(MMBaseContext.getConfigPath() + File.separator + "media" + File.separator + "mimemapping.xml");
@@ -52,23 +52,23 @@ public final class Format {   // final class!!
             };
         watcher.add(mimeMappingFile);
         watcher.start();
-     
+
     }
-    
+
     static void readMimeMapping(File mimeMappingFile) {
         mimeMapping = new HashMap();
-        
+
 
         if (mimeMappingFile.canRead()) {
             log.service("Reading " + mimeMappingFile);
             XMLBasicReader reader = new XMLBasicReader(mimeMappingFile.toString(), Format.class);
-            
+
             for(Iterator e = reader.getChildElements("mimemapping", "map"); e.hasNext();) {
                 Element map = (Element)e.next();
                 String format = reader.getElementAttributeValue(map, "format");
                 String codec = reader.getElementAttributeValue(map, "codec");
                 String mime = reader.getElementValue(map);
-                
+
                 mimeMapping.put(format + "/" + codec,mime);
                 log.debug("Adding mime mapping " + format + "/" + codec + " -> " + mime);
             }
@@ -78,23 +78,23 @@ public final class Format {   // final class!!
     }
 
     private static List formats = new ArrayList(); // to make possible to get the Format object by int.
-    private int    number; // for storage     
+    private int    number; // for storage
     private String id;     // for toString(), and as identifier in config file etc.
                            // Also sync with common extension?
                            // perhaps this could as well be used for storage
-    
-    
+
+
     private Format(int n, String i) { // private constructor!!
-        number = n; id = i; 
+        number = n; id = i;
         while (formats.size() <= number) formats.add(UNKNOWN);
         formats.set(number, this);
-    }         
-    
+    }
+
     // in contradiction to the example of the cited URL I prefer
     // to state the number explicitely, because those numbers will
     // appear in the database, so never may change (so don't
     // determin the number automaticly
-    
+
     public static final Format UNKNOWN = new Format(0, "unknown");
     public static final Format MP3  = new Format(1, "mp3");
     public static final Format RA   = new Format(2, "ra");
@@ -137,6 +137,7 @@ public final class Format {   // final class!!
     public static final Format M4V = new Format(61, "m4v");
 
     public static final Format GGP = new Format(70, "3gpp");
+    public static final Format FLASH = new Format(80, "swf");
 
     public int toInt()    { return number; }
     public String toString() { return id;     }
@@ -152,7 +153,7 @@ public final class Format {   // final class!!
      * don't know if this is nice
      */
     public static List getMediaFormats() {
-        return Arrays.asList(new Format[] {MP3, RA, RA,WAV, PCM, MP2, RM, VOB, AVI, MPEG, MP4, MPG, ASF, MOV, WMA, OGG, OGM, RAM, WMP, QT, ASX, WAX, WMV, WVX, WM, WMZ, WMD, MID, PODCAST, VODCAST, M4A, M4V, GGP});
+        return Arrays.asList(new Format[] {MP3, RA, RA,WAV, PCM, MP2, RM, VOB, AVI, MPEG, MP4, MPG, ASF, MOV, WMA, OGG, OGM, RAM, WMP, QT, ASX, WAX, WMV, WVX, WM, WMZ, WMD, MID, PODCAST, VODCAST, M4A, M4V, GGP, FLASH});
     }
     public static Format get(String id) {
         id = id.toLowerCase();
@@ -166,7 +167,7 @@ public final class Format {   // final class!!
         log.warn("Unknown media format '" + id + "'. Returning " + UNKNOWN);
         return UNKNOWN;
     }
-    
+
     public String getGUIIndicator(Locale locale) {
         try {
             ResourceBundle m = ResourceBundle.getBundle(RESOURCE, locale);
@@ -178,7 +179,7 @@ public final class Format {   // final class!!
     }
 
 
-    protected static final List windowsMedia = Arrays.asList(new Format[] {ASF, WMP, WMA, ASX,  WAX, WMV, WVX, WM, WMX, WMZ, WMD});    
+    protected static final List windowsMedia = Arrays.asList(new Format[] {ASF, WMP, WMA, ASX,  WAX, WMV, WVX, WM, WMX, WMZ, WMD});
     protected static final List real         = Arrays.asList(new Format[] {RA, RM, RAM});
 
     public boolean isReal() {
@@ -208,7 +209,7 @@ public final class Format {   // final class!!
         if(codec == null || codec.equals("")) {
             codec = "*";
         }
-        
+
         String mimeType = (String) mimeMapping.get(format + "/" + codec);
         while (mimeType == null) {
             if (! codec.equals("*")) {
@@ -238,8 +239,8 @@ public final class Format {   // final class!!
         }
         return false;
     }
-    
-    
+
+
     /**
      * @see java.lang.Object#hashCode()
      */
@@ -247,4 +248,4 @@ public final class Format {   // final class!!
         return number;
     }
 }
-    
+
