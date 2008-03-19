2cffb44a4b4f4fc6319f8bbfaab58c5203fa496f
wrosdorff <wrosdorff@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-03-19 11:36:45 +0000

    now the first level menu's are also real pages

--- mmbase/branches/b1_4/contributions/CMSContainer_Demo/demo.cmscontainer.org/war/src/webapp/js/nav.js
+++ mmbase/branches/b1_4/contributions/CMSContainer_Demo/demo.cmscontainer.org/war/src/webapp/js/nav.js
@@ -2,8 +2,8 @@ addLoadEvent(zetNav)
 
 function zetNav(){
 	document.body.onclick = function(){
-		toonNav(this);
-	
+		if(this.parentNode.tagName == 'LI') return;
+		hideSubNavs();
 	}
 	for(i=1;i<6;i++){
 		navparent = document.getElementById('nav0'+i);
@@ -14,18 +14,14 @@ function zetNav(){
 			navparent = document.getElementById('navtext0'+i);
 		}	
 		if (navparent != null && navparent.hasChildNodes()) {
+			navparent.onmouseover = function(){
+				onMenuOver(this);
+			}
 			nav1=navparent.childNodes;		
 			for(j=0;j<nav1.length;j++){
 				if(nav1[j].nodeType == 1 && nav1[j].tagName == 'A'){
-					nav1[j].onclick = function(e){
-						toonNav(this);
-						if (!e) var e = window.event;
-						e.cancelBubble = true;
-						if (e.stopPropagation) e.stopPropagation();
-						return false;
-					}
 					nav1[j].onmouseover = function(){
-						toonSub(this);
+						onSubMenuOver(this);
 					}
 				}
 			}
@@ -33,43 +29,50 @@ function zetNav(){
 	}
 }
 
-aan = 0;
-function toonNav(dit){
-	if(aan == 0){
-		if(dit.parentNode.tagName != 'LI') return;
-		aan = 1;
-	}
-	else if(aan == 1){
-		aan = 0;
-	}
-	toonSub(dit);
+function onMenuOver(dit){
+	parentstring = dit.id;
+	toonSub(dit, parentstring);
 }
 
-function toonSub(dit){
+function onSubMenuOver(dit){
+	dit.className = 'active';
+}
+
+function toonSub(dit, parentstring){
+	hideSubNavs();
+	deactivateAnchors();
+	
+	beginIndex = parentstring.lastIndexOf("0");
+	endIndex = parentstring.length;
+	lastdigits = parentstring.substring(beginIndex, endIndex);		
+	if (document.getElementById('subnav'+lastdigits) != null) {		
+		document.getElementById('subnav'+lastdigits).style.display = 'block';
+	}
+}
+
+function getDeNav(){
 	denav = document.getElementById('nav');
 	if(denav == null){
 		denav = document.getElementById('navrecprod');
 	}
 	if (denav == null){
 		denav = document.getElementById('navtext');
-	}	
-	deas = denav.getElementsByTagName('A');
-	for(i=0;i<deas.length;i++){
-		deas[i].className = '';
 	}
+	return denav;
+}
+
+function hideSubNavs(){
 	for(j=1;j<6;j++){
 		if (document.getElementById('subnav0'+j) != null) {
 			document.getElementById('subnav0'+j).style.display = 'none';
 		}
 	}
-	if(aan==1){
-		dit.className = 'active';
-		parentstring = dit.parentNode.id;
-		beginIndex = parentstring.lastIndexOf("0");
-		endIndex = parentstring.length;
-		lastdigits = parentstring.substring(beginIndex, endIndex);		
-		if (document.getElementById('subnav'+lastdigits) != null) {		
-			document.getElementById('subnav'+lastdigits).style.display = 'block';
-		}		
+}
+
+function deactivateAnchors(){
+	denav = getDeNav();
+	deas = denav.getElementsByTagName('A');
+	for(i=0;i<deas.length;i++){
+		deas[i].className = '';
 	}
 }
