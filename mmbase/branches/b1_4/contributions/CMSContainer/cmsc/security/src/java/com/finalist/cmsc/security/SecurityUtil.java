829cebac99fd4055f82e7832fb49efa86cef5331
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-06-26 16:38:22 +0000

    CMSC-973, it will now correctly merge the rights of multiple groups.

--- mmbase/branches/b1_4/contributions/CMSContainer/cmsc/security/src/java/com/finalist/cmsc/security/SecurityUtil.java
+++ mmbase/branches/b1_4/contributions/CMSContainer/cmsc/security/src/java/com/finalist/cmsc/security/SecurityUtil.java
@@ -154,14 +154,32 @@ public class SecurityUtil {
 		            Node extraGroup = groups.getNode(i);
 		            fillChannelsWithRole(extraGroup, extraRoles, treeManagers, relationName);
 
-		            for (Map.Entry<String, UserRole> entry : extraRoles.entrySet()) {
-		                String extraPath = entry.getKey();
-		                UserRole extraRole = entry.getValue();
-		                UserRole channelRole = getRole(extraPath, channelsWithRole);
-		                if (channelRole == null || channelRole.getRole().getId() < extraRole.getRole().getId()) {
-		                    channelsWithRole.put(extraPath, extraRole);
-		                }
-		            }
+		            // upgrade user role map based on the data of the new group data map
+                  for (Map.Entry<String, UserRole> entry : channelsWithRole.entrySet()) {
+                     String path = entry.getKey();
+                     UserRole userRole = entry.getValue();
+                     UserRole newGroupRole = getRole(path, extraRoles);
+                     if (newGroupRole != null && userRole.getRole().getId() < newGroupRole.getRole().getId()) {
+                         channelsWithRole.put(path, newGroupRole);
+                     }
+                 }
+                 
+                 // add missing data to user role map based on new group data map
+                 for (Map.Entry<String, UserRole> entry : extraRoles.entrySet()) {
+                     String extraPath = entry.getKey();
+                     UserRole newGroupRole = entry.getValue();
+                     UserRole channelRole = getRole(extraPath, channelsWithRole);
+                     Role userRole;
+                     if (channelRole == null) {
+                        userRole = Role.NONE;
+                     }
+                     else {
+                        userRole = channelRole.getRole();
+                     }
+                     if (userRole.getId() < newGroupRole.getRole().getId()) {
+                         channelsWithRole.put(extraPath, newGroupRole);
+                     }
+                 }
 		        }
 		    }
 		}
