26ef26098925a9bbfe84e469e79ff68e35566b85
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2008-11-06 08:40:08 +0000

    CMSC-948 Registration portlet for CMS container

--- mmbase/branches/b1_5/contributions/CMSContainer_Portlets/portlets-login/src/java/com/finalist/cmsc/login/ConfirmAction.java
+++ mmbase/branches/b1_5/contributions/CMSContainer_Portlets/portlets-login/src/java/com/finalist/cmsc/login/ConfirmAction.java
@@ -3,13 +3,10 @@ package com.finalist.cmsc.login;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.mmapps.modules.cloudprovider.CloudProviderFactory;
-
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.mmbase.bridge.Cloud;
 import org.mmbase.util.Encode;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -19,7 +16,6 @@ import com.finalist.cmsc.services.community.person.Person;
 import com.finalist.cmsc.services.community.person.PersonService;
 import com.finalist.cmsc.services.community.person.RegisterStatus;
 import com.finalist.cmsc.services.community.security.AuthenticationService;
-import com.finalist.cmsc.services.publish.Publish;
 import com.finalist.cmsc.util.HttpUtil;
 
 public class ConfirmAction extends Action{
