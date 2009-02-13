83990ea54e4f8bbba00b2b6c5b8ee1fced71152b
nklasens <nklasens@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-02-13 21:26:02 +0000

    CMSC-1289 Create a readonly instance of the CMSc Movinf ServerUtil to utilities

--- mmbase/branches/b1_5/contributions/CMSContainer_Modules/publish-cleaner/src/java/com/finalist/cmsc/publish/CleanerModule.java
+++ mmbase/branches/b1_5/contributions/CMSContainer_Modules/publish-cleaner/src/java/com/finalist/cmsc/publish/CleanerModule.java
@@ -12,9 +12,9 @@ import org.mmbase.remotepublishing.PublishManager;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
-import com.finalist.cmsc.navigation.NavigationUtil;
-import com.finalist.cmsc.navigation.PagesUtil;
+import com.finalist.cmsc.navigation.*;
 import com.finalist.cmsc.repository.ContentElementUtil;
+import com.finalist.cmsc.util.ServerUtil;
 
 /**
  * Cleans (deletes) nodes in mmbase when they are expired
@@ -46,6 +46,11 @@ public class CleanerModule extends Module implements Runnable {
     */
    public void init() {
       mmb = MMBase.getMMBase();
+      
+      if (ServerUtil.isReadonly()) {
+         return;
+      }
+      
       // Initialize the module.
       String intervalStr = getInitParameter("interval");
       if (intervalStr == null) {
@@ -55,6 +60,8 @@ public class CleanerModule extends Module implements Runnable {
          interval = Integer.parseInt(intervalStr) * 1000;
       }
 
+      
+      
       // Start thread to wait for mmbase to be up and running.
       Thread cleaner = new Thread(this);
       cleaner.setDaemon(true);
