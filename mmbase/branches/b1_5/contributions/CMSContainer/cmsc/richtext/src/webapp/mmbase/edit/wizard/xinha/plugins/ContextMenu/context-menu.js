85e0e136fd3cc6d42eacd35964bffbc766e1614e
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-03-27 11:02:34 +0000

    CMSC-1226 - Context menu in Rich Text editor is displayed at the wrong place in FF

--- mmbase/branches/b1_5/contributions/CMSContainer/cmsc/richtext/src/webapp/mmbase/edit/wizard/xinha/plugins/ContextMenu/context-menu.js
+++ mmbase/branches/b1_5/contributions/CMSContainer/cmsc/richtext/src/webapp/mmbase/edit/wizard/xinha/plugins/ContextMenu/context-menu.js
@@ -6,7 +6,7 @@
 // Distributed under the same terms as HTMLArea itself.
 // This notice MUST stay intact for use (see license.txt).
 //
-// $Id: context-menu.js,v 1.1 2009-03-16 11:07:52 raintang Exp $
+// $Id: context-menu.js,v 1.1.6.1 2009-03-27 11:02:34 jkoster Exp $
 
 HTMLArea.loadStyle("menu.css", "ContextMenu");
 
