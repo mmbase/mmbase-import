e6937281abdb45d8699b8e0eb88bde1ee9b5cf7f
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-04-27 02:33:54 +0000

    CMSC-1386  Newsletter module spams the logfile

--- mmbase/branches/b1_6/contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/NewsletterCronJob.java
+++ mmbase/branches/b1_6/contributions/CMSContainer_Portlets/portlets-newsletter/src/java/com/finalist/newsletter/NewsletterCronJob.java
@@ -228,6 +228,7 @@ public class NewsletterCronJob extends AbstractCronJob {
 
    @Override
    public void init() {
+      log.info("Start Newsletter CronJob");
       NewsletterService newsletterService = (NewsletterService) ApplicationContextFactory.getBean("newsletterServices");
       BounceChecker checker = new BounceChecker(newsletterService);
       if (!checker.isRunning() && (ServerUtil.isStaging() || ServerUtil.isSingle())) {
@@ -244,7 +245,7 @@ public class NewsletterCronJob extends AbstractCronJob {
             newsletterNode.setDateValue("lastcreateddate", new Date());
             newsletterNode.commit();
             int newsletterNumber = newsletterNode.getNumber();
-            log.info("Running Newsletter CronJob for newsletter " + newsletterNumber);
+            log.debug("Running Newsletter CronJob for newsletter " + newsletterNumber);
             //NewsletterPublicationUtil.createPublication(newsletterNumber, true);
             Node publicationNode = NewsletterPublicationUtil.createPublication(newsletterNumber, true);
             NewsletterUtil.addNewsletterCreationChannel(newsletterNode.getNumber(),publicationNode.getNumber());
