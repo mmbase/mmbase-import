a45e88647ad310f1ab95204e8b7dcd22306ee22f
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-01 15:24:22 +0000

    moved branches

--- mmbase/tags/Root_with-mmbase-1-7-3/contributions/didactor2/src/education/java/nl/didactor/education/builders/QuestionBuilder.java
+++
@@ -1,8 +0,0 @@
-package nl.didactor.education.builders;
-import org.mmbase.module.core.MMObjectBuilder;
-import org.mmbase.module.core.MMObjectNode;
-
-public abstract class QuestionBuilder extends nl.didactor.versioning.builders.LOVersioningBuilder {
-
-    public abstract int getScore(MMObjectNode questionNode, MMObjectNode givenAnswer);
-}
