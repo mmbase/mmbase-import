f8f555a82b14ee37409569a7b25c08acacf08c4d
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-01-15 11:22:55 +0000

    Fixing compile errors I am having in eclipse

--- mmbase/branches/MMBase-1_9/applications/media/src/main/java/org/mmbase/applications/media/MimeType.java
+++ mmbase/branches/MMBase-1_9/applications/media/src/main/java/org/mmbase/applications/media/MimeType.java
@@ -9,9 +9,6 @@ See http://www.MMBase.org/license
 */
 package org.mmbase.applications.media;
 
-import org.mmbase.util.logging.*;
-import org.mmbase.bridge.*;
-
 
 /**
  *
@@ -19,7 +16,7 @@ import org.mmbase.bridge.*;
  */
 
 public class MimeType implements java.io.Serializable {
-    private static long serialVersionUID = 0L;
+    private static final long serialVersionUID = 0L;
 
     public static final String STAR = "*";
     public static final MimeType ANY = new MimeType(STAR, STAR);
