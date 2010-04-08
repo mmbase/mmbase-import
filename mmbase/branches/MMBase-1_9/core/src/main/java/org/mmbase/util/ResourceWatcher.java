73a2230349cba3c7990d2422727a0861aff94c56
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-04-08 10:50:00 +0000

    getResources returns an unmodifiable

--- mmbase/branches/MMBase-1_9/core/src/main/java/org/mmbase/util/ResourceWatcher.java
+++ mmbase/branches/MMBase-1_9/core/src/main/java/org/mmbase/util/ResourceWatcher.java
@@ -301,7 +301,7 @@ public abstract class ResourceWatcher implements NodeEventListener  {
         if (running) { // it's simplest like this.
             exit();
         }
-        getResources().remove(resourceName);
+        resources.remove(resourceName);
         if (wasRunning) {
             start();
         }
