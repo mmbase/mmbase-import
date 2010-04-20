88193e9a56d07290ae60117fa6ffe9186986068b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-04-20 13:12:18 +0000

    MMB-1653 (default searchdir)

--- mmbase/branches/MMBase-1_9/core/src/main/java/org/mmbase/module/core/MMBaseContext.java
+++ mmbase/branches/MMBase-1_9/core/src/main/java/org/mmbase/module/core/MMBaseContext.java
@@ -14,6 +14,7 @@ import java.io.*;
 import javax.servlet.*;
 import java.text.DateFormat;
 
+import org.mmbase.core.event.*;
 import org.mmbase.core.util.DaemonTask;
 import org.mmbase.core.util.DaemonThread;
 import org.mmbase.util.ResourceLoader;
@@ -66,6 +67,8 @@ public class MMBaseContext {
 
             // store the current context
             sx = servletContext;
+            EventManager.getInstance().propagateEvent(new SystemEvent.ServletContext(sx));
+
             // Get the user directory using the user.dir property.
             // default set to the startdir of the appserver
             userDir = sx.getInitParameter("user.dir");
