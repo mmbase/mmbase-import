f1146aa44a1fc6f54424d9ea7c6f8023a7b2b81e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-05-18 11:49:41 +0000

    made the ServetContex event asynchronous

--- mmbase/branches/MMBase-1_9/core/src/main/java/org/mmbase/module/core/MMBaseContext.java
+++ mmbase/branches/MMBase-1_9/core/src/main/java/org/mmbase/module/core/MMBaseContext.java
@@ -67,7 +67,8 @@ public class MMBaseContext {
 
             // store the current context
             sx = servletContext;
-            EventManager.getInstance().propagateEvent(new SystemEvent.ServletContext(sx));
+            log.service("Found servletContext " + sx);
+            EventManager.getInstance().propagateEvent(new SystemEvent.ServletContext(sx), true);
 
             // Get the user directory using the user.dir property.
             // default set to the startdir of the appserver
