57f95d78c7c9bf11f85b286f16bb1c095303a1ec
Michiel Meeuwissen <git@meeuw.org>
2010-05-18 14:41:01 +0000

    Added SystemEvent.BuildersReader

--- core/src/main/java/org/mmbase/bridge/mock/MockCloudContext.java
+++ core/src/main/java/org/mmbase/bridge/mock/MockCloudContext.java
@@ -18,6 +18,7 @@ import org.mmbase.datatypes.DataType;
 import org.mmbase.bridge.implementation.*;
 import org.mmbase.storage.search.*;
 import org.mmbase.security.*;
+import org.mmbase.core.event.*;
 import org.mmbase.util.*;
 import org.mmbase.util.logging.*;
 import org.xml.sax.InputSource;
@@ -108,6 +109,7 @@ public class MockCloudContext extends  AbstractCloudContext {
                 LOG.service("Builder with name '" + buil + "' already exists");
             }
         }
+        EventManager.getInstance().propagateEvent(new SystemEvent.BuildersRead(this));
     }
 
     protected int getTypeDefNode(String name) {
@@ -123,6 +125,7 @@ public class MockCloudContext extends  AbstractCloudContext {
         }
 
         nodeManagers.put(name, new NodeManagerDescription(name, m, getTypeDefNode(name)));
+        EventManager.getInstance().propagateEvent(new SystemEvent.BuildersRead(this));
     }
 
     public void addNodeManager(InputSource source) {
@@ -130,6 +133,7 @@ public class MockCloudContext extends  AbstractCloudContext {
             MockBuilderReader reader = new MockBuilderReader(source, this);
             addNodeManager(reader);
         }
+        EventManager.getInstance().propagateEvent(new SystemEvent.BuildersRead(this));
     }
 
     protected void addNodeManager(MockBuilderReader reader) {
@@ -153,6 +157,7 @@ public class MockCloudContext extends  AbstractCloudContext {
                 }
             }
         }
+        EventManager.getInstance().propagateEvent(new SystemEvent.BuildersRead(this));
     }
 
     public synchronized int addNode(String type, Map<String, Object> map) {
