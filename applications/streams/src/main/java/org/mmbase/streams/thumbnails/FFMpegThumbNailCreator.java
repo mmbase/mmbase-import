24605a1bfc1fa28a07ef7d2995933dfda70e6389
André van Toly <513194+elfuego@users.noreply.github.com>
2012-11-06 16:11:54 +0000

    MMB-2036 executors were not split bewteen transcoders and recognizers leading to assigning them all to be transcoders and filling slots for recognizers stalling uploads

--- applications/streams/src/main/java/org/mmbase/streams/thumbnails/FFMpegThumbNailCreator.java
+++ applications/streams/src/main/java/org/mmbase/streams/thumbnails/FFMpegThumbNailCreator.java
@@ -28,6 +28,7 @@ import java.io.*;
 
 import org.mmbase.streams.createcaches.Executors;
 import org.mmbase.bridge.*;
+import org.mmbase.streams.createcaches.Stage;
 import org.mmbase.util.logging.*;
 import org.mmbase.util.externalprocess.*;
 import org.mmbase.util.WriterOutputStream;
@@ -86,7 +87,7 @@ public class FFMpegThumbNailCreator implements  Callable<Long> {
             LOG.debug("File is empty " + input);
             return null;
         }
-        CommandExecutor.Method method = Executors.getFreeExecutor();
+        CommandExecutor.Method method = Executors.getFreeExecutor(Stage.RECOGNIZER);    // get a recognizer one, they are shortest in use
         String command = "ffmpeg";
         List<String> args = new ArrayList<String>();
         args.add("-i");
