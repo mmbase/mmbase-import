4b564c6cdc0a6a43336e9ef96b74bd59d5feba70
Michiel Meeuwissen <git@meeuw.org>
2014-05-09 15:00:44 +0000

    1.8.8

--- src/org/mmbase/Version.java
+++ src/org/mmbase/Version.java
@@ -97,7 +97,7 @@ public class Version {
      * @since MMBase-1.6
      */
     public static boolean isRelease() {
-        return false;
+        return true;
     };
 
     /**
@@ -106,7 +106,7 @@ public class Version {
      * @since MMBase-1.7
      */
     public static String getReleaseStatus() {
-        return "";
+        return "final";
     };
 
     /**
