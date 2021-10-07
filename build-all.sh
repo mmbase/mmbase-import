bab748990c5687d774da2b48613aece69b06e82b
Michiel Meeuwissen <git@meeuw.org>
2021-10-07 23:28:12 +0200

    debugging.

--- build-all.sh
+++ build-all.sh
@@ -6,8 +6,11 @@ export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Sl
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 #MVN="mvn -Dcom.sun.net.ssl.checkRevocation=false -Dhttps.protocols=TLSv1.2 -Duser.home=$DIR"
 SETTINGS=${GITHUB_WORKSPACE:-$DIR}/.github/settings.xml
+cat $SETTINGS
+echo $OSSRH_PASSWORD | md5sum
 MVN="mvn $MAVEN_OPTS -s $SETTINGS -Dgpg.skip=true -B"
 
+$MVN effective-pom
 for d in  . maven-base maven maven/maven-mmbase-plugin maven-base/applications applications   ; do
     (cd $DIR/$d &&  $MVN -N clean deploy)
 done
